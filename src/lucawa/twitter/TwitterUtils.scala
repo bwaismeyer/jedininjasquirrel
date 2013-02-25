afa7a8da8ee99ed4ad3eff36830a637a59a11fe0
Chris Lucas <cglucas@gmail.com>
2013-02-24 23:39:07 -0500

    word count stub, related tools

--- src/lucawa/twitter/TwitterUtils.scala
+++ src/lucawa/twitter/TwitterUtils.scala
@@ -279,36 +279,11 @@ object TwitterUtils {
     val twitterStream:TwitterStream = (new TwitterStreamFactory(config)).getInstance();
     var filterQuery = new  FilterQuery()
     twitterStream.addListener(listener)
+    twitterStream.addListener(new WordCountListener)
     filterQuery = filterQuery.track(keywords.toArray)
     twitterStream.filter(filterQuery)
     Thread.sleep(5000)
-   
   }
   
 }
 
-class PrintStatusListener extends StatusListener {
- def onStatus(status:Status) {
-            System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-        }
-
-def onDeletionNotice(statusDeletionNotice:StatusDeletionNotice) {
-            System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
-        }
-
-def onTrackLimitationNotice(numberOfLimitedStatuses:Int) {
-            System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
-        }
-
-def onScrubGeo(userId:Long,upToStatusId:Long) {
-            System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
-        }
-
-def onStallWarning(sw:StallWarning) = {
-  throw new RuntimeException(sw.getMessage())
-}
-
-def onException(ex:Exception) {
-            ex.printStackTrace();
-        }
-}
