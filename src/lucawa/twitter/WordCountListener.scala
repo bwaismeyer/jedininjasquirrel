7c38dc3d79736f70f59bfee886c3c718453b7329
Chris Lucas <cglucas@gmail.com>
2013-03-13 17:48:29 -0400

    forgot to add some files

---
+++ src/lucawa/twitter/WordCountListener.scala
@@ -0,0 +1,41 @@
+package lucawa.twitter
+
+import java.io.FileInputStream
+import java.io.ObjectInputStream
+import java.io.FileOutputStream
+import java.io.ObjectOutputStream
+import twitter4j._
+import twitter4j.conf._
+import scala.collection.JavaConversions._
+import scala.collection.mutable.ArrayBuffer
+
+class WordCountListener extends StatusListener {
+  
+  
+	def onStatus(status:Status) {
+		System.out.print("@" + status.getUser().getScreenName() + " - ")
+		val st = status.getText()
+		val wordArray = st.split("[\\s\\.:\\(\\)\\*\\!]+")
+		println(WordCounter.countWords(wordArray))
+	}
+
+	def onDeletionNotice(statusDeletionNotice:StatusDeletionNotice) {
+		System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+	}
+
+	def onTrackLimitationNotice(numberOfLimitedStatuses:Int) {
+		System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+	}
+
+	def onScrubGeo(userId:Long,upToStatusId:Long) {
+		System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+	}
+
+	def onStallWarning(sw:StallWarning) = {
+		throw new RuntimeException(sw.getMessage())
+	}
+
+	def onException(ex:Exception) {
+		ex.printStackTrace();
+	}
+}
---
+++ src/lucawa/twitter/WordCounter.scala
@@ -0,0 +1,48 @@
+package lucawa.twitter
+import clTools.WeightedCollection
+import scala.io.Source
+import java.io.File
+
+object WordCounter {
+
+  val masterCounts = new WeightedCollection[String](true,false)
+  var initialized = false;
+  
+  def main(args:Array[String]) = {
+    init
+  }
+  
+  def init = {
+    val files = new File("output").listFiles().filter(f => f.isFile() && f.getName().startsWith("slice") ) //Vector("output/slice-#.txt")
+    var lineNum:Long = 0 
+    for(f <- files) {
+      val source = Source.fromFile(f)
+      val lines = source.getLines
+      while(lines.hasNext) {
+        val line:IndexedSeq[String] = lines.next.split("\t")
+        masterCounts.add(cleanWord(line(0)),line(1).toDouble)
+        lineNum = lineNum+1
+        if(lineNum % 100000 == 0) {
+          println("Processed line number %d, file is %s, counts has %10.0f elems".format(lineNum,f.toString,masterCounts.totalWeight))
+        }
+      }
+    }
+    initialized=true
+    //println("Processed line number %d, counts has %10.0f elems, %10d keys".format(lineNum,masterCounts.totalWeight,masterCounts.getItems.size))
+  }
+  
+  def cleanWord(w:String) = {
+    w.toLowerCase().replaceAll("[\\.\\!\\?,'`\"]*","")
+  }
+  
+  /**
+   * Returns counts for all of the words in the IndexedSeq.
+   * 
+   * Currently a stub -- does not actually work, but instead returns all zeros.
+   * 
+   */
+  def countWords(ws:IndexedSeq[String]):IndexedSeq[Int] = {
+    for(w <- ws) yield masterCounts.getWeight(cleanWord(w)).toInt
+  }
+  
+}
